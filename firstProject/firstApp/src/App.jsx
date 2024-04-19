12b21b335e28663ea289a0fff18c672dc835f961
Evan Jelley <jelleyevan@gmail.com>
2024-04-19 14:14:15 -0600

    Restructure

--- firstProject/firstApp/src/App.jsx
+++ firstProject/firstApp/src/App.jsx
@@ -1,189 +1,130 @@
 /* eslint-disable */
-import * as React from 'react'
-import './App.css'
-import axios from 'axios'
+import * as React from 'react';
+import axios from 'axios';
+
+import { SearchForm } from './SearchForm';
+import { List } from './List';
+
+const storiesReducer = (state, action) => {
+  switch (action.type) {
+    case 'STORIES_FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case 'STORIES_FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'STORIES_FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case 'REMOVE_STORY':
+      return {
+        ...state,
+        data: state.data.filter(
+          (story) => action.payload.objectID !== story.objectID
+        ),
+      };
+    default:
+      throw new Error();
+  }
+};
+
+const useStorageState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
-const getSumComments = stories => {
-  console.log('Calculating the sum of comments');
-  return stories.data.reduce(
-    (result, value) => result + value.num_comments,
-    0
-  )
-}
-
-
 const App = () => {
-  const storiesReducer = (state, action) => {
-    switch (action.type) {
-      case 'STORIES_FETCH_INIT':
-        return {
-          ...state,
-          isLoading: true,
-          isError: false,
-        };
-      case 'STORIES_FETCH_SUCCESS':
-        return {
-          ...state,
-          isLoading: false,
-          isError: false,
-          data: action.payload
-        }
-      case 'STORIES_FETCH_FAILURE':
-        return {
-          ...state,
-          isLoading: false,
-          isError: true,
-        };
-      case 'REMOVE_STORY':
-        return {
-          ...state,
-          data: state.data.filter(story => action.payload.objectID !== story.objectID),
-        }
-      default:
-        throw new Error();
-    }
-  }
+  const [searchTerm, setSearchTerm] = useStorageState(
+    'search',
+    'React'
+  );
+
+  const [url, setUrl] = React.useState(
+    `${API_ENDPOINT}${searchTerm}`
+  );
 
   const [stories, dispatchStories] = React.useReducer(
     storiesReducer,
     { data: [], isLoading: false, isError: false }
   );
 
-  const [searchTerm, setSearchTerm] = React.useState(localStorage.getItem('search') || '');
-
-  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
-
-  const handleSearchInput = event => {
-    setSearchTerm(event.target.value)
-  };
-
-  const handleSearchSubmit = (event) => {
-    setUrl(`${API_ENDPOINT}${searchTerm}`);
-    event.preventDefault();
-  };
-
   const handleFetchStories = React.useCallback(async () => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
     try {
-      const result = await axios.get(url)
+      const result = await axios.get(url);
+
       dispatchStories({
         type: 'STORIES_FETCH_SUCCESS',
         payload: result.data.hits,
       });
     } catch {
-      dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
-    };
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
+    }
   }, [url]);
 
   React.useEffect(() => {
     handleFetchStories();
   }, [handleFetchStories]);
 
-  const handleRemoveStory = React.useCallback(item => {
+  const handleRemoveStory = (item) => {
     dispatchStories({
       type: 'REMOVE_STORY',
       payload: item,
     });
-  }, []);
+  };
 
-  React.useEffect(() => {
-    localStorage.setItem('search', searchTerm)
-  }, [searchTerm])
-
-  const handleSearch = event => {
+  const handleSearchInput = (event) => {
     setSearchTerm(event.target.value);
   };
 
-  console.log('Rendering App');
+  const handleSearchSubmit = (event) => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
 
-  const sumComments = React.useMemo(() =>
-    getSumComments(stories),
-    [stories]
-  );
+    event.preventDefault();
+  };
 
   return (
-    <div className='container'>
-      <h1 className='headline-primary'>My Hacker Stories</h1>
-      <p>Total Comments: {sumComments}</p>
+    <div>
+      <h1>My Hacker Stories</h1>
 
-      <SearchForm searchTerm={searchTerm} onSearchInput={handleSearchInput} onSearchSubmit={handleSearchSubmit} />
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
+
+      <hr />
 
       {stories.isError && <p>Something went wrong ...</p>}
 
-      {stories.isLoading ? (<p>Loading...</p>) :
-        (<List list={stories.data} onRemoveItem={handleRemoveStory} />)
-      }
-
+      {stories.isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
+      )}
     </div>
-  )
-}
-
-const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => {
-  return (
-    <form onSubmit={onSearchSubmit} className='search-form'>
-      <InputWithLabel
-        id="search"
-        value={searchTerm}
-        onInputChange={onSearchInput}
-        isFocused
-      >
-        <strong>Search: </strong>
-      </InputWithLabel>
-
-      <button
-        type="submit"
-        disabled={!searchTerm}
-        className='button button_large'
-      >
-        Submit
-      </button>
-    </form>
-  )
-}
-
-const List = React.memo(
-  ({ list, onRemoveItem }) => {
-    console.log('Rendering List')
-    return (
-      < ul >
-        {list.map(item => (
-          <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />))}
-      </ul >
-    );
-  }
-);
-
-const Item = ({ item, onRemoveItem }) => {
-  return (
-    <li className='item'>
-      <span style={{ width: '40%' }}>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span style={{ width: '30%' }}>{item.author}</span>
-      <span style={{ width: '10%' }}>{item.num_comments}</span>
-      <span style={{ width: '10%' }}>{item.points}</span>
-      <span style={{ width: '10%' }}>
-        <button className='button button_small' type="button" onClick={() => onRemoveItem(item)}>
-          Delete
-        </button>
-      </span>
-    </li>
-  )
-}
-
-
-const InputWithLabel = ({ id, value, type = "text", onInputChange, isFocused, children }) => {
-  return (
-    <>
-      <label htmlFor={id} className='label'>{children}</label>
-      <input id={id} type={type} autoFocus={isFocused} onChange={onInputChange} value={value} className='input' />
-    </>
-  )
-}
+  );
+};
 
 export default App;
 
-export { SearchForm, InputWithLabel, List, Item};
--- firstProject/firstApp/src/App.test.jsx
+++ firstProject/firstApp/src/App.test.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable */
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
+import axios from 'axios';
 
 import App, {
     Item,
@@ -14,6 +15,8 @@ import {
     fireEvent,
     waitFor,
 } from '@testing-library/react';
+
+vi.mock('axios');
 
 describe('something truthy and falsy', () => {
     it('true to be true', () => {
@@ -46,7 +49,6 @@ const stories = [storyOne, storyTwo];
 describe('Item', () => {
     it('renders all properties', () => {
         render(<Item item={storyOne} />);
-        screen.debug();
         expect(screen.getByText('React')).toBeTruthy();
         expect(screen.getByText('Jordan Walke')).toBeTruthy();
     });
@@ -56,10 +58,64 @@ describe('Item', () => {
         expect(screen.getByRole('button')).toBeTruthy();
     });
 
-    // it('clicking the dismiss button calls the callback handler', () => {
-    //     const handleRemoveItem = jest.fn();
-    //     render(<Item item={storyOne} onRemoveItem={handleRemoveItem} />);
-    //     fireEvent.click(screen.getByRole('button'));
-    //     expect(handleRemoveItem).toHaveBeenCalledTimes(1);
-    // });
+    it('clicking the dismiss button calls the callback handler', () => {
+        const handleRemoveItem = vi.fn();
+        render(<Item item={storyOne} onRemoveItem={handleRemoveItem} />);
+        fireEvent.click(screen.getByRole('button'));
+        expect(handleRemoveItem).toHaveBeenCalledTimes(1);
+    });
+})
+
+
+describe('SearchForm', () => {
+    const searchFormProps = {
+        searchTerm: 'React',
+        onSearchInput: vi.fn(),
+        onSearchSubmit: vi.fn(),
+    };
+
+    it('renders the input field with its value', () => {
+        render(<SearchForm {...searchFormProps} />);
+        expect(screen.getByDisplayValue('React')).toBeInTheDocument();
+    });
+
+
+    it('calls onSearchInput on input field change', () => {
+        render(<SearchForm {...searchFormProps} />);
+        fireEvent.change(screen.getByDisplayValue('React'), {
+            target: { value: 'Redux' },
+        });
+        expect(searchFormProps.onSearchInput).toHaveBeenCalledTimes(1);
+    });
+
+    it('calls onSearchSubmit on button submit click', () => {
+        render(<SearchForm {...searchFormProps} />);
+        fireEvent.submit(screen.getByRole('button'));
+        expect(searchFormProps.onSearchSubmit).toHaveBeenCalledTimes(1);
+    });
+});
+
+describe('App', () => {
+    it('succeeds fetching data', async () => {
+        const promise = Promise.resolve({
+            data: { hits: stories },
+        });
+        axios.get.mockImplementationOnce(() => promise);
+        render(<App />);
+        await waitFor(() => [
+            expect(screen.getByText('React')).toBeTruthy(),
+            expect(screen.getByText('Redux')).toBeTruthy(),
+        ]);
+    });
+
+    it('fails fetching data', async () => {
+        const promise = Promise.reject();
+        axios.get.mockImplementationOnce(() => promise);
+        render(<App />);
+        await waitFor(() => [
+            expect(screen.queryByText('React')).toBeNull(),
+            expect(screen.queryByText('Redux')).toBeNull(),
+            expect(screen.getByText('Something went wrong ...')).toBeTruthy(),
+        ]);
+    });
 })
